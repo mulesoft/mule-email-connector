1218401bacb1e0d1197b4dc6f642b7388f651449
fabian.gonzalez <fabian.gonzalez@mulesoft.com>
2018-10-11 14:42:07 -0300

    MULE-15797: IMAP folder is closed before the last item in a page is consumed.

---
+++ src/main/java/org/mule/extension/email/internal/util/DefaultMailPartContentResolver.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.email.internal.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.mail.MessagingException;
+import javax.mail.Part;
+
+/**
+ * Default resolver for a mail part.
+ * 
+ * @since 1.2.0
+ */
+public class DefaultMailPartContentResolver implements MailPartContentResolver {
+
+  private static final MailPartContentResolver[] RESOLVERS =
+      new MailPartContentResolver[] {
+          new IMAPMailPartContentResolver()
+      };
+
+  @Override
+  public InputStream resolveInputStream(Part part) throws IOException, MessagingException {
+    for (MailPartContentResolver resolver : RESOLVERS) {
+      if (resolver.resolvesType(part)) {
+        return resolver.resolveInputStream(part);
+      }
+    }
+    return part.getInputStream();
+  }
+
+  @Override
+  public boolean resolvesType(Part part) {
+    return true;
+  }
+
+}
---
+++ src/test/java/org/mule/extension/email/util/DefaultMailPartContentResolverTestCase.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.email.util;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.apache.commons.io.IOUtils.toByteArray;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.mule.extension.email.internal.util.DefaultMailPartContentResolver;
+import org.mule.extension.email.internal.util.MailPartContentResolver;
+
+import com.sun.mail.imap.IMAPMessage;
+
+public class DefaultMailPartContentResolverTestCase {
+
+  private static final String CONTENT_PAYLOAD = "CONTENT";
+  private static final String CONTENT_FILE = "unit/content.txt";
+
+  @Test
+  public void resolveIMAPPartContentToByteArrayStream() throws Exception {
+    try (InputStream imapInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(CONTENT_FILE)) {
+      MailPartContentResolver resolver = new DefaultMailPartContentResolver();
+      IMAPMessage message = mock(IMAPMessage.class);
+      when(message.getInputStream()).thenReturn(imapInputStream);
+      InputStream content = resolver.resolveInputStream(message);
+      assertThat(content, instanceOf(ByteArrayInputStream.class));
+      assertThat(new String(toByteArray(content)), equalTo(CONTENT_PAYLOAD));
+    }
+  }
+
+  @Test
+  public void resolveMessagePartContentToOriginalStreamClass() throws Exception {
+    try (InputStream imapInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(CONTENT_FILE)) {
+      MailPartContentResolver resolver = new DefaultMailPartContentResolver();
+      MimeMessage message = mock(MimeMessage.class);
+      when(message.getInputStream()).thenReturn(imapInputStream);
+      InputStream content = resolver.resolveInputStream(message);
+      assertThat(content, instanceOf(imapInputStream.getClass()));
+      assertThat(new String(toByteArray(content)), equalTo(CONTENT_PAYLOAD));
+    }
+  }
+
+
+}
