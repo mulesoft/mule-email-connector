d6c073c603534107f30d5d2d5a8dba9399b8a3ba
Alejandro García Marra <alejandro.garcia@mulesoft.com>
2020-01-06 16:39:05 -0300

    MULE-17925: Attachment name not found for Outlook sent attachments (#141)
    (cherry picked from commit 055041568792d5f69655ad0a699ddf2005788c98)

--- src/test/java/org/mule/extension/email/unit/StoredEmailContentTestCase.java
+++ src/test/java/org/mule/extension/email/unit/StoredEmailContentTestCase.java
@@ -7,18 +7,14 @@
 
 package org.mule.extension.email.unit;
 
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyListOf;
-import static org.mockito.Matchers.isNotNull;
-import static org.mockito.Matchers.notNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-import com.google.common.collect.ImmutableMap;
 import org.mule.extension.email.api.StoredEmailContent;
-import org.mule.extension.email.internal.DefaultStoredEmailContent;
 import org.mule.extension.email.internal.StoredEmailContentFactory;
 import org.mule.runtime.api.metadata.TypedValue;
 import org.mule.runtime.extension.api.runtime.streaming.StreamingHelper;
@@ -40,17 +36,37 @@ public class StoredEmailContentTestCase {
     InputStream multipart = Thread.currentThread().getContextClassLoader().getResourceAsStream("unit/multipart");
     StreamingHelper helper = mock(StreamingHelper.class);
     when(helper.resolveCursorProvider(any())).thenAnswer(a -> a.getArgumentAt(0, InputStream.class));
-    Message message = mock(Message.class);
-    when(message.getContent()).thenReturn(multipart);
-    when(message.getContentType()).thenReturn("multipart/mixed; boundary=\"f403045e6d18904495056a4ab7e8\"");
-    when(message.isMimeType("multipart/*")).thenReturn(true);
-    when(message.isMimeType("multipart/alternative")).thenReturn(false);
-    when(message.isMimeType("multipart/related")).thenReturn(false);
-    when(message.isMimeType("multipart/mixed")).thenReturn(true);
+    Message message = mockMessage(multipart, "multipart/mixed; boundary=\"f403045e6d18904495056a4ab7e8\"");
     StoredEmailContent content = new StoredEmailContentFactory(helper).fromMessage(message);
     Map<String, TypedValue<InputStream>> attachments = content.getAttachments();
     assertThat(attachments.size(), is(1));
     TypedValue<InputStream> csv = attachments.get("input.csv");
     assertThat(IOUtils.toString(csv.getValue()), is("orderId,name,units,pricePerUnit\r\n1,aaa,2.0,10\r\n2,bbb,4.15,5"));
+  }
+
+  @Test
+  public void inputStreamContentFromOutlook() throws IOException, MessagingException {
+    InputStream multipart = Thread.currentThread().getContextClassLoader().getResourceAsStream("unit/outlook_multipart");
+    StreamingHelper helper = mock(StreamingHelper.class);
+    when(helper.resolveCursorProvider(any())).thenAnswer(a -> a.getArgumentAt(0, InputStream.class));
+    Message message = mockMessage(multipart, "multipart/mixed;\n"
+        + "boundary=\"----=_NextPart_000_0039_01D5B1D7.2E5205B0\"");
+    StoredEmailContent content = new StoredEmailContentFactory(helper).fromMessage(message);
+    Map<String, TypedValue<InputStream>> attachments = content.getAttachments();
+    assertThat(attachments.size(), is(2));
+    TypedValue<InputStream> image = attachments.get("image001.jpg");
+    assertThat(image.getDataType().getMediaType().toString(), is("image/jpeg"));
+    assertThat(image.getValue(), not(nullValue()));
+  }
+
+  private Message mockMessage(InputStream multipart, String contentType) throws IOException, MessagingException {
+    Message message = mock(Message.class);
+    when(message.getContent()).thenReturn(multipart);
+    when(message.getContentType()).thenReturn(contentType);
+    when(message.isMimeType("multipart/*")).thenReturn(true);
+    when(message.isMimeType("multipart/alternative")).thenReturn(false);
+    when(message.isMimeType("multipart/related")).thenReturn(false);
+    when(message.isMimeType("multipart/mixed")).thenReturn(true);
+    return message;
   }
 }
