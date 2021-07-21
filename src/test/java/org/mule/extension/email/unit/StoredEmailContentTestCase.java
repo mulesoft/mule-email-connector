fdaf40aecc11a2d21d805ebfefaccdb4e69cafe9
Alejandro Curci <76498083+acurci-at-mulesoft@users.noreply.github.com>
2021-07-21 14:02:55 -0300

    SE-20527: Add unnamed attachment test

--- src/test/java/org/mule/extension/email/unit/StoredEmailContentTestCase.java
+++ src/test/java/org/mule/extension/email/unit/StoredEmailContentTestCase.java
@@ -62,6 +62,18 @@ public class StoredEmailContentTestCase {
   }
 
   @Test
+  public void multipartMixed_test_Unnamed() throws IOException, MessagingException {
+    InputStream multipart = Thread.currentThread().getContextClassLoader().getResourceAsStream("unit/multipart_no_name_and_filename");
+    StreamingHelper helper = mock(StreamingHelper.class);
+    when(helper.resolveCursorProvider(any())).thenAnswer(a -> a.getArgument(0));
+    Message message = mockMessage(multipart, "multipart/mixed; boundary=\"f403045e6d18904495056a4ab7e8\"");
+    StoredEmailContent content = new StoredEmailContentFactory(helper).fromMessage(message, NAME_HEADERS);
+    Map<String, TypedValue<InputStream>> attachments = content.getAttachments();
+    assertThat(attachments.size(), is(1));
+    assertThat(attachments.get("Unnamed"), not(nullValue()));
+  }
+
+  @Test
   public void multipartMixed_test() throws IOException, MessagingException {
     InputStream multipart = Thread.currentThread().getContextClassLoader().getResourceAsStream("unit/multipart_basic");
     StreamingHelper helper = mock(StreamingHelper.class);
