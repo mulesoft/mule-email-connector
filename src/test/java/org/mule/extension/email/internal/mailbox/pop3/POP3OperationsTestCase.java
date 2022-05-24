df2e1b562f1dea4085eac44366383dd64ae2f51b
Ignacio Giss <rgiss@mulesoft.com>
2022-05-24 16:28:06 -0300

    W-10915411: change test and fix format

--- src/test/java/org/mule/extension/email/internal/mailbox/pop3/POP3OperationsTestCase.java
+++ src/test/java/org/mule/extension/email/internal/mailbox/pop3/POP3OperationsTestCase.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.extension.email.internal.mailbox.pop3;
 
 import static org.hamcrest.Matchers.is;
@@ -17,25 +23,26 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class POP3OperationsTestCase {
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
 
-    private static POP3Operations pop3Operations;
-    private static MailboxConnection connection;
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  private static POP3Operations pop3Operations;
+  private static MailboxConnection connection;
 
 
-    @BeforeClass
-    public static void setup() {
-        pop3Operations = new POP3Operations();
-        connection = mock(MailboxConnection.class);
-    }
+  @BeforeClass
+  public static void setup() {
+    pop3Operations = new POP3Operations();
+    connection = mock(MailboxConnection.class);
+  }
 
-    @Test
-    public void countFail() throws MessagingException {
-        when(connection.getDefaultFolder()).thenThrow(FolderNotFoundException.class);
-        expectedException.expect(EmailCountMessagesException.class);
-        expectedException.expectMessage(is("Error while counting messages in the INBOX folder"));
-        pop3Operations.countMessagesPop3(connection);
-    }
+  @Test
+  public void countFail() throws MessagingException {
+    when(connection.getDefaultFolder()).thenThrow(FolderNotFoundException.class);
+    expectedException.expect(EmailCountMessagesException.class);
+    expectedException.expectMessage(is("Error while counting messages in the INBOX folder"));
+    pop3Operations.countMessagesPop3(connection);
+  }
 
 }
