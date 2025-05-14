4efc0c8b01aba3ef11ca8b4535766c2937672dbd
juan.flores <juan.flores@mulesoft.com>
2025-05-14 14:50:03 -0300

    apply format

--- src/test/java/org/mule/extension/email/sender/EmailBodyTestCase.java
+++ src/test/java/org/mule/extension/email/sender/EmailBodyTestCase.java
@@ -12,31 +12,33 @@ import static org.junit.Assert.assertEquals;
 
 public class EmailBodyTestCase {
 
-    @Test
-    public void testGetContentAsString() throws Exception{
-        String expectedContent = "This a Mock body Message";
-        byte[] contentBytes = expectedContent.getBytes(StandardCharsets.UTF_8);
-        InputStream emailBodyStream =  new ByteArrayInputStream(contentBytes);
-        TypedValue<InputStream> content = new TypedValue<>(emailBodyStream,null);
-        EmailBody emailBody = new EmailBody(content,"text/plain","UTF-8");
-        String result =  emailBody.getContentAsString(StandardCharsets.UTF_8);
-        assertEquals(expectedContent,result);
-    }
-    @Test
-    public void testGetContentRepeatableResultAsString() throws Exception{
-        String bodyMessage = "This a Mock body Message";
-        byte[] contentBytes = bodyMessage.getBytes(StandardCharsets.UTF_8);
-        InputStream emailBodyStream =  new ByteArrayInputStream(contentBytes);
-        TypedValue<InputStream> content = new TypedValue<>(emailBodyStream,null);
-        EmailBody emailBody = new EmailBody(content,"text/plain","UTF-8");
-        String result =  emailBody.getContentAsString(StandardCharsets.UTF_8);
-        String repeatableResult = emailBody.getContentAsString(StandardCharsets.UTF_8);
-        assertEquals(result,repeatableResult);
-    }
-    @Test
-    public void testGetContentNullAsString() throws Exception{
-        EmailBody emailBody = new EmailBody(null,"text/plain","UTF-8");
-        String result =  emailBody.getContentAsString(StandardCharsets.UTF_8);
-        assertEquals("",result);
-    }
+  @Test
+  public void testGetContentAsString() throws Exception {
+    String expectedContent = "This a Mock body Message";
+    byte[] contentBytes = expectedContent.getBytes(StandardCharsets.UTF_8);
+    InputStream emailBodyStream = new ByteArrayInputStream(contentBytes);
+    TypedValue<InputStream> content = new TypedValue<>(emailBodyStream, null);
+    EmailBody emailBody = new EmailBody(content, "text/plain", "UTF-8");
+    String result = emailBody.getContentAsString(StandardCharsets.UTF_8);
+    assertEquals(expectedContent, result);
+  }
+
+  @Test
+  public void testGetContentRepeatableResultAsString() throws Exception {
+    String bodyMessage = "This a Mock body Message";
+    byte[] contentBytes = bodyMessage.getBytes(StandardCharsets.UTF_8);
+    InputStream emailBodyStream = new ByteArrayInputStream(contentBytes);
+    TypedValue<InputStream> content = new TypedValue<>(emailBodyStream, null);
+    EmailBody emailBody = new EmailBody(content, "text/plain", "UTF-8");
+    String result = emailBody.getContentAsString(StandardCharsets.UTF_8);
+    String repeatableResult = emailBody.getContentAsString(StandardCharsets.UTF_8);
+    assertEquals(result, repeatableResult);
+  }
+
+  @Test
+  public void testGetContentNullAsString() throws Exception {
+    EmailBody emailBody = new EmailBody(null, "text/plain", "UTF-8");
+    String result = emailBody.getContentAsString(StandardCharsets.UTF_8);
+    assertEquals("", result);
+  }
 }
