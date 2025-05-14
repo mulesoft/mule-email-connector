2c7eacc264af47770dc6c69728504f81b081330f
juan.flores <juan.flores@mulesoft.com>
2025-05-14 14:45:51 -0300

    Added unit test for Emailbody.getContentAsString

---
+++ src/test/java/org/mule/extension/email/sender/EmailBodyTestCase.java
@@ -0,0 +1,42 @@
+package org.mule.extension.email.sender;
+
+import org.junit.Test;
+import org.mule.extension.email.internal.sender.EmailBody;
+import org.mule.runtime.api.metadata.TypedValue;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+public class EmailBodyTestCase {
+
+    @Test
+    public void testGetContentAsString() throws Exception{
+        String expectedContent = "This a Mock body Message";
+        byte[] contentBytes = expectedContent.getBytes(StandardCharsets.UTF_8);
+        InputStream emailBodyStream =  new ByteArrayInputStream(contentBytes);
+        TypedValue<InputStream> content = new TypedValue<>(emailBodyStream,null);
+        EmailBody emailBody = new EmailBody(content,"text/plain","UTF-8");
+        String result =  emailBody.getContentAsString(StandardCharsets.UTF_8);
+        assertEquals(expectedContent,result);
+    }
+    @Test
+    public void testGetContentRepeatableResultAsString() throws Exception{
+        String bodyMessage = "This a Mock body Message";
+        byte[] contentBytes = bodyMessage.getBytes(StandardCharsets.UTF_8);
+        InputStream emailBodyStream =  new ByteArrayInputStream(contentBytes);
+        TypedValue<InputStream> content = new TypedValue<>(emailBodyStream,null);
+        EmailBody emailBody = new EmailBody(content,"text/plain","UTF-8");
+        String result =  emailBody.getContentAsString(StandardCharsets.UTF_8);
+        String repeatableResult = emailBody.getContentAsString(StandardCharsets.UTF_8);
+        assertEquals(result,repeatableResult);
+    }
+    @Test
+    public void testGetContentNullAsString() throws Exception{
+        EmailBody emailBody = new EmailBody(null,"text/plain","UTF-8");
+        String result =  emailBody.getContentAsString(StandardCharsets.UTF_8);
+        assertEquals("",result);
+    }
+}
